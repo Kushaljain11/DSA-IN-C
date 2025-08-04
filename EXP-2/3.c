7078d84543dc1ced09775ab3c663e711661095b3
Kushal  Jain <kushaljain112006@gmail.com>
2025-08-04 09:45:30 +0530

    Update 3.c

--- EXP-2/3.c
+++ EXP-2/3.c
@@ -1,4 +1,25 @@
-//3
+// 3) WAP in C to perform swapping of two numbers by passing address of the variables to the functions.
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    struct Student s1 = {"Kushal", 002, 87.5};
+    struct Student *ptr;
+    ptr = &s1;
+
+    printf("Student Details:\n");
+    printf("Name: %s\n", ptr.name);
+    printf("Roll Number: %d\n", ptr.roll);
+    printf("Marks: %.2f\n", ptr.marks);
+
+    return 0;
+}
+
 #include <stdio.h>
 
 void swap(int *x, int *y) {
@@ -21,3 +42,4 @@ int main() {
 
     return 0;
 }
+
